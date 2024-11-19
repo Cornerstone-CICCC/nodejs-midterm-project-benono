4e899020c256cbc058b93c4ff7b4b839eb36b831
BenOno <bentoki.1213@gmail.com>
2024-11-18 16:31:17 -0800

    fix(frontend): fix color

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -31,7 +31,7 @@ const HomePage = () => {
   }, [subscribeToNewMatches, unsubscribeFromNewMatches, authUser]);
 
   return (
-    <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-pink-100 to-purple-100 overflow-hidden">
+    <div className="flex flex-col lg:flex-row min-h-screen bg-gradient-to-br from-indigo-100 to-purple-100 overflow-hidden">
       <Sidebar />
       <div className="flex-grow flex flex-col overflow-hidden">
         <Header />
@@ -55,7 +55,7 @@ export default HomePage;
 
 const NoMoreProfiles = () => (
   <div className="flex flex-col items-center justify-center h-full text-center p-8">
-    <Frown className="text-pink-400 mb-6" size={80} />
+    <Frown className="text-indigo-400 mb-6" size={80} />
     <h2 className="text-3xl font-bold text-gray-800 mb-4">
       Woah there, speedy fingers!
     </h2>
@@ -72,7 +72,7 @@ const LoadingUI = () => {
         <div className="px-4 pt-4 h-3/4">
           <div className="w-full h-full bg-gray-200 rounded-lg" />
         </div>
-        <div className="card-body bg-gradient-to-b from-white to-pink-50 p-4">
+        <div className="card-body bg-gradient-to-b from-white to-indigo-50 p-4">
           <div className="space-y-2">
             <div className="h-6 bg-gray-200 rounded w-3/4" />
             <div className="h-4 bg-gray-200 rounded w-1/2" />
